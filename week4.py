0ccfe7796546509e611231b94a1365c0c4fb40ce
Suarez <vssuarez1@cps.k12.il.us>
2024-09-18 09:09:31 -0500

    first commit

---
+++ week4.py
@@ -0,0 +1,48 @@
+# Advanced Problem Set: Delving Deeper with Numbers in Python --- Bell Ringer
+
+#NAMES: Vanessa Suarez, Malik Hreish 
+
+#################### PROBLEM 1: Basic Arithmetic & Order of Operations ####################
+# Task 1: Using the order of operations (PEMDAS/BODMAS), evaluate the following expression:
+# 3 + 6 * (5 + 4) / 3 - 7. Print the result.
+print(14)
+
+
+# Task 2: Calculate the remainder when 145 is divided by 12 using modulo and print the result.
+print(145%12
+
+# Task 3: Raise 7 to the power of 3 and print the result.
+print(7^3)
+
+#################### PROBLEM 2: Working with Functions ####################
+# Task 4: Given a list of numbers:
+numbers = [23, 89, 12, 54, 92, 65, 71, 13, 45]
+# Use the max() and min() functions to find the highest and lowest number respectively.
+print("the max of 23,89,12,54,92,65,71,13,45 is" ,max(23,89,12,54,92,65,71,13,45))
+print("the min of 23,89,12,54,92,65,71,13,45 is" ,min(23,89,12,54,92,65,71,13,45))
+
+# Task 5: Round the number 12.5678 to 2 decimal places.
+print("round 12.5678 is",round (12.5678))
+
+# Task 6: Find the absolute value of -45.
+print("the absoulute value of -45 is", abs(-45))
+
+#################### PROBLEM 3: Advanced Math Functions ####################
+from math import *
+
+# Task 7: Using the math library, find the floor value of 15.7.
+print("The floor of 15.7 is" ,floor(15.7))
+
+# Task 8: Find the ceiling value of 15.2.
+print("The ceil of 15.2 is" ,ceil(15.2))
+
+# Task 9: Calculate the square root of 625.
+print("The square root of 625 is" ,sqrt(625))
+#################### PROBLEM 4: Problem Solving ####################
+# Task 10: You are given two lists:
+prices = [34.56, 45.78, 23.89, 12.34, 78.90]
+quantities = [3, 5, 2, 4, 6]
+# Calculate the total cost for each item (price multiplied by quantity).
+# Then, find the average cost of all items after rounding it to 2 decimal places.
+print("The prices summed up together is" ,sum(prices))
+#################### END OF ADVANCED PROBLEM SET  -- end Bell Ringer  ####################
